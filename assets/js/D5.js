d673be966e0df1dd017c2fc21556489c297a5330
antoniocostantin <costantini062@gmail.com>
2024-06-21 13:10:48 +0200

    pausa pranzo

--- assets/js/D5.js
+++ assets/js/D5.js
@@ -10,19 +10,43 @@ REGOLE
 /* ESERCIZIO 1
     Dato il seguente array, scrivi del codice per stampare ogni elemento dell'array in console.
 */
-const pets = ['dog', 'cat', 'hamster', 'redfish']
+const pets = ['dog', 'cat', 'hamster', 'redfish'];
+console.log(">>>>>>>>>>ESERCIO 1<<<<<<<<<");
+
+for (let i = 0; i < pets.length; i++) {
+  console.log(pets[i]);
+}
 
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
 */
+console.log(">>>>>>>>>>ESERCIO 2<<<<<<<<<");
+let pets2 = pets.sort();
+console.log(pets2);
 
 /* ESERCIZIO 3
     Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
 */
+console.log(">>>>>>>>>>ESERCIO 3<<<<<<<<<");
+let j = 0;
+const petsRev = [];
 
+for (let i = pets.length - 1; i >= 0; i--){
+  petsRev[j] = pets2[i];
+  j++;
+}
+
+console.log(petsRev);
 /* ESERCIZIO 4
     Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
 */
+console.log(">>>>>>>>>>ESERCIO 4<<<<<<<<<");
+
+for(let i = 0; i < petsRev.length; i++){
+  if(i === 0){
+  }
+}
+
 
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
